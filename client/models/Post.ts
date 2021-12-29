fd657e45eb4e3917a11039f477aa5cebbbbe6565
mono-tmt <tomita@monosus.co.jp>
2021-12-29 16:02:57 +0900

    :sparkles: add post api for demo

---
+++ client/models/Post.ts
@@ -0,0 +1,12 @@
+export type Post = {
+    id: number;
+    title: string;
+    content: string;
+    created_at: any;
+    updated_at: any;
+    published_at: any;
+}
+
+export type PostJsonResponse = {
+    data: Post[];
+}
